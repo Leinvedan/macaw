fc9a268d4e5791d6458b6b00f57df379a678828e
Leinvedan <fontebasso.lemos@gmail.com>
2022-03-27 19:44:37 -0300

    refactor: separate extractor tests to mach file structure

---
+++ tests/extractor_links_tests.py
@@ -0,0 +1,36 @@
+import unittest
+from macaw.extractors.links import extract_links
+from macaw.configs import HREF_XPATH, SCRIPT_XPATH, PageType
+
+FIXTURE_PATH = 'tests/fixtures'
+RESULT_PATH = 'tests/results'
+
+
+class ExtractLinkTestCase(unittest.TestCase):
+    def setUp(self):
+        with open(f'{FIXTURE_PATH}/vultr_1.html', 'r') as f:
+            self.vultr_1 = f.read()
+        with open(f'{FIXTURE_PATH}/docean_1.html', 'r') as f:
+            self.docean_1 = f.read()
+
+    def test_extract_link_vultr_1(self):
+        keywords = ['/pricing', 'cloud']
+        expected_links = ['/pricing/cloud', '/pricing/#cloud-compute/']
+
+        links = extract_links(self.vultr_1, HREF_XPATH, keywords)
+
+        self.assertEqual(len(links), len(expected_links))
+        self.assertTrue(expected_links[0] in links)
+        self.assertTrue(expected_links[1] in links)
+
+    def test_extract_link_docean_1(self):
+        keywords = ['/_next/static/chunks/pages/pricing-']
+        expected_link = '/_next/static/chunks/pages/pricing-1a7cdb1f9a255535.js'
+
+        links = extract_links(self.docean_1, SCRIPT_XPATH, keywords)
+
+        self.assertEqual(len(links), 1)
+        self.assertTrue(expected_link in links)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/extractor_plans_tests.py
+++ tests/extractor_plans_tests.py
@@ -1,7 +1,6 @@
 import unittest
 from macaw.extractors.plans import extract_plans
-from macaw.extractors.links import extract_links
-from macaw.configs import HREF_XPATH, SCRIPT_XPATH, PageType
+from macaw.configs import PageType
 
 FIXTURE_PATH = 'tests/fixtures'
 RESULT_PATH = 'tests/results'
@@ -14,25 +13,6 @@ class ExtractTestCase(unittest.TestCase):
         with open(f'{FIXTURE_PATH}/docean_1.html', 'r') as f:
             self.docean_1 = f.read()
 
-    def test_extract_link_vultr_1(self):
-        keywords = ['/pricing', 'cloud']
-        expected_links = ['/pricing/cloud', '/pricing/#cloud-compute/']
-
-        links = extract_links(self.vultr_1, HREF_XPATH, keywords)
-
-        self.assertEqual(len(links), len(expected_links))
-        self.assertTrue(expected_links[0] in links)
-        self.assertTrue(expected_links[1] in links)
-
-    def test_extract_link_docean_1(self):
-        keywords = ['/_next/static/chunks/pages/pricing-']
-        expected_link = '/_next/static/chunks/pages/pricing-1a7cdb1f9a255535.js'
-
-        links = extract_links(self.docean_1, SCRIPT_XPATH, keywords)
-
-        self.assertEqual(len(links), 1)
-        self.assertTrue(expected_link in links)
-
     def test_extract_prices_all_values(self):
         prices = extract_plans(self.vultr_1, PageType.HTML)
         total_machines = 16
@@ -42,7 +22,6 @@ class ExtractTestCase(unittest.TestCase):
         with open(f'{RESULT_PATH}/vultr_1.txt', 'r') as f:
             expected = f.read()
 
-        # Quando mudar pra json o diff vai ficar melhor
         self.assertEqual(str(prices), expected)
         self.assertEqual(len(prices), total_machines)
 
