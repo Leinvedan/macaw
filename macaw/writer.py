e10f446adf95e31925634b75feded0c9461d4fde
Leinvedan <fontebasso.lemos@gmail.com>
2022-03-29 20:08:02 -0300

    feat: ask for input if no args are given

--- macaw/writer.py
+++ macaw/writer.py
@@ -14,39 +14,54 @@ def get_writer_function() -> Callable:
     in the respective output of the given commandline argument.
     '''
 
+    command = ''
     if len(sys.argv) < 2:
         logging.warning('Please choose a output argument')
-        print_help()
-        sys.exit(1)
+        _print_help()
+        command = _get_command()
+    else:
+        command = sys.argv[1]
 
-    match sys.argv[1]:
+    match command:
         case '--help':
-            print_help()
+            _print_help()
             sys.exit(0)
         case '--print':
             return print
         case '--save_json':
-            return write_as_json
+            return _write_as_json
         case '--save_csv':
-            return write_as_csv
+            return _write_as_csv
         case _:
             logging.error('Invalid argument')
-            print_help()
+            _print_help()
             sys.exit(1)
 
+def _get_command():
+    try:
+        while True:
+            command = input('command: ')
+            if command in ['--print', '--save_json', '--save_csv']:
+                break
+    except KeyboardInterrupt:
+        logging.info('Quitting application...')
+        sys.exit(0)
 
-def print_help():
+    return command
+
+
+def _print_help():
     logging.info('--print: prints the result in stdout')
     logging.info('--save_json: saves the result as plans.json file')
 
 
-def write_as_json(data: list[dict[str, str]]):
+def _write_as_json(data: list[dict[str, str]]):
     result = json.dumps(data, indent=2)
     with open(f'{OUT_FILENAME}.json', 'w', encoding='UTF-8') as file:
         file.write(result)
 
 
-def write_as_csv(data: list[dict[str, str]]):
+def _write_as_csv(data: list[dict[str, str]]):
     with open(f'{OUT_FILENAME}.csv', 'w', encoding='UTF-8') as file:
         writer = csv.DictWriter(file, fieldnames=FIELD_NAMES)
         writer.writeheader()
