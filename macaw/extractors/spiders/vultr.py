6a4e2a7f4740910fb22c760c96a56fb9aa9e77c1
Leinvedan <fontebasso.lemos@gmail.com>
2022-03-28 22:34:54 -0300

    refactor: vultr functions

--- macaw/extractors/spiders/vultr.py
+++ macaw/extractors/spiders/vultr.py
@@ -27,29 +27,44 @@ def parse_html(html: str) -> list[dict[str, str]]:
     selector = Selector(html)
 
     plan_nodes = selector.css(row_css).getall()
-    for idx, plan in enumerate(plan_nodes):
-        selector = Selector(plan)
 
-        resource_nodes = selector.css(price_css).getall()
+    for idx, plan in enumerate(plan_nodes):
+        plan_selector = Selector(plan)
+        resource_nodes = plan_selector.css(price_css).getall()
+
         for resource in resource_nodes:
             value = re.search(NUMBER_REGEX, resource)
             value_unit = re.search(UNIT_REGEX, resource)
             resource_type = re.search(ASSET_REGEX, resource, re.IGNORECASE)
 
-            if value and resource_type:
-                resource_type = resource_type.group()
-                value = value.group()
+            if not value or not resource_type:
+                continue
 
-                if value_unit:
-                    value = f'{value}{value_unit.group()}'
+            value, resource_type = _parse_results(value, resource_type, value_unit)
 
-                if resource_type.lower() in ['cpu', 'vcpu']:
-                    value = f'{value}{resource_type}'
+            is_fist_price_of_resource = len(extracted) - 1 < idx
+            if is_fist_price_of_resource:
+                extracted.append({})
 
-
-                if len(extracted) - 1 < idx:
-                    extracted.append({})
-
-                extracted[idx][f'{resource_type}'] = value
+            extracted[idx][f'{resource_type}'] = value
 
     return extracted
+
+def _parse_results(value, resource_type, value_unit) -> tuple[str, str]:
+    '''
+    Parse the regex results to variables, example:
+    - value = 1GB
+    - resource_type = Memory
+
+    returns tuple: `(value, resource_type)`
+    '''
+    resource_type = resource_type.group()
+    value = value.group()
+
+    if value_unit:
+        value = f'{value}{value_unit.group()}'
+
+    if resource_type.lower() in ['cpu', 'vcpu']:
+        value = f'{value}{resource_type}'
+    return (value, resource_type)
+    
